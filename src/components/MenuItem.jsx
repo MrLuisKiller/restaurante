c552fe73de3820a7d15907841375607fa217a96f
MrLuisKiller <luis_bol_1998@hotmail.com>
2023-12-05 21:20:00 -0700

    Footer fixed

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -4,10 +4,10 @@ const MenuItem = ({ Menu }) => {
     const { imagen, comida, descripcion, precio } = Menu
 
     return (
-        <div className="card mb-3 col-lg-6 col-md-12" style={{ maxWidth: 540 }}>
+        <div className="card mb-3 col-lg-6 col-md-12 border-0" style={{ maxWidth: 540 }}>
             <div className="row g-2">
                 <div className="col-md-4">
-                    <img src={`./src/assets/Menu/${imagen}`} className='img-fluid rounded-start' alt="Imagen de la comida" />
+                    <img src={`./src/assets/Menu/${imagen}`} className='img-fluid rounded shadow-lg' alt="Imagen de la comida" />
                 </div>
                 <div className="col-md-8">
                     <h5 className="card-title pt-2">{comida}</h5>
